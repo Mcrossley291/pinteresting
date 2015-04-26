ef058ec54115f4500d024c88041229bcf8e49957
Micah Crossley <micah@t3designs.com>
2015-04-26 01:10:38 -0700

    Added Devise gem and followed steps to setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.default_url_options = { :host => 'app.t3designs.com' }
+  # Required for Heroku
+  # Note to set this to our own domain name
 end
